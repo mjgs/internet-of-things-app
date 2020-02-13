74533e447a3b972c8a27878ca8447c3b72ee23f0
Mark Smith <markjgsmith@gmail.com>
2020-02-14 04:19:07 +0700

    Update method used to set NODE_ENV in tests

--- test/integration/integration.test.js
+++ test/integration/integration.test.js
@@ -5,7 +5,16 @@ const expect = require('chai').expect;
 const request = require('supertest');
 const cheerio = require('cheerio');
 
-const app = require('../../lib/app');
+let app;
+
+beforeEach(function(done) {
+  app = require('../../lib/app');
+  return done();
+});
+
+afterEach(function() {
+  process.env.NODE_ENV = 'test';
+});
 
 it('should test that the integration test suite is setup', function(done) {
   expect(true).to.be.true; // eslint-disable-line no-unused-expressions
@@ -13,7 +22,9 @@ it('should test that the integration test suite is setup', function(done) {
 });
 
 it('should load the 404 error page in development mode', function(done) {
-  app.set('env', 'development');
+  process.env.NODE_ENV = 'development';
+  delete require.cache[require.resolve('../../lib/app')]; // clear module from require cache
+  app = require('../../lib/app'); // reload module so env gets set
   request(app)
     .get('/pagedoesnotexist')
     .expect('Content-Type', 'text/html; charset=utf-8')
@@ -22,13 +33,14 @@ it('should load the 404 error page in development mode', function(done) {
       if (err) {
         return done(err);
       }
-      app.set('env', 'development');
       return done();
     });
 });
 
 it('should load the 404 error page in production mode', function(done) {
-  app.set('env', 'production');
+  process.env.NODE_ENV = 'production';
+  delete require.cache[require.resolve('../../lib/app')]; // clear module from require cache
+  app = require('../../lib/app'); // reload module so env gets set
   request(app)
     .get('/pagedoesnotexist')
     .expect('Content-Type', 'text/html; charset=utf-8')
@@ -37,12 +49,30 @@ it('should load the 404 error page in production mode', function(done) {
       if (err) {
         return done(err);
       }
-      app.set('env', 'development');
       return done();
     });
 });
 
 it('should load the dashboard page', function(done) {
+  request(app)
+    .get('/')
+    .expect('Content-Type', 'text/html; charset=utf-8')
+    .expect(200)
+    .end(function(err, res) {
+      if (err) {
+        return done(err);
+      }
+      const $ = cheerio.load(res.text);
+      expect($('h1')[0].children[0].data).to.equal('Dashboard');
+      expect($('p')[0].children[0].data).to.equal('Welcome to Dashboard');
+      return done();
+    });
+});
+
+it('should load the dashboard page in development mode', function(done) {
+  process.env.NODE_ENV = 'development';
+  delete require.cache[require.resolve('../../lib/app')]; // clear module from require cache
+  app = require('../../lib/app'); // reload module so env gets set
   request(app)
     .get('/')
     .expect('Content-Type', 'text/html; charset=utf-8')

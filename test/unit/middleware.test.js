862f52bd6c4cc3a460f1e43122d908437742a7ea
Mark Smith <markjgsmith@gmail.com>
2020-02-15 14:34:24 +0700

    Refactor middleware unit tests and add tests for bad params

--- test/unit/middleware.test.js
+++ test/unit/middleware.test.js
@@ -14,55 +14,151 @@ describe('#middleware', function() {
   });
 
   describe('#logRequest', function() {
-    it('should log the request', function() {
-      // setup
-      const req = {
-        baseUrl: '',
-        body: {},
-        cookies: {},
-        fresh: false,
-        headers: {},
-        hostname: 'localhost',
-        ip: '127.0.0.1',
-        ips: [],
-        method: 'GET',
-        originalUrl: '/',
-        params: {},
-        path: '/',
-        protocol: 'http',
-        query: {},
-        route: {},
-        secure: false,
-        signedCookies: {},
-        stale: true,
-        subdomains: [],
-        xhr: false
-      };
-      const res = {};
-      const next = sinon.fake(test);
-      const debugStub = sinon.stub();
+    const mockReqFixture = {
+      baseUrl: '',
+      body: {},
+      cookies: {},
+      fresh: false,
+      headers: {},
+      hostname: 'localhost',
+      ip: '127.0.0.1',
+      ips: [],
+      method: 'GET',
+      originalUrl: '/',
+      params: {},
+      path: '/',
+      protocol: 'http',
+      query: {},
+      route: {},
+      secure: false,
+      signedCookies: {},
+      stale: true,
+      subdomains: [],
+      xhr: false
+    };
+
+    let logRequestMiddleware, debugStub, utilsStub;
+
+    beforeEach(function() {
+      debugStub = sinon.stub();
       const debugFake = sinon.fake(function(namespace) {
         return debugStub;
       });
-      const utilsStub = {
+      utilsStub = {
         getRequestInfo: sinon.stub()
       };
       const stubs = {
         debug: debugFake,
         '../utils': utilsStub
       };
-      const logRequestMiddleware = proxyquire('../../lib/middleware/logRequest', stubs);
+      logRequestMiddleware = proxyquire('../../lib/middleware/logRequest', stubs);
+    });
+
+    it('should log the request', function() {
+      // setup
+      const mockReq = Object.assign({}, mockReqFixture);
+      const mockRes = {};
+      const mockNext = sinon.fake(test);
 
       // run
-      logRequestMiddleware(req, res, next);
+      logRequestMiddleware(mockReq, mockRes, mockNext);
 
       // test
       function test(err) {
         expect(err).to.be.undefined;
         expect(utilsStub.getRequestInfo.calledOnce).to.be.true;
-        expect(utilsStub.getRequestInfo.calledWith(req)).to.be.true;
+        expect(utilsStub.getRequestInfo.calledWith(mockReq)).to.be.true;
         expect(debugStub.calledOnce).to.be.true;
-        expect(next.calledOnce).to.be.true;
+        expect(mockNext.calledOnce).to.be.true;
+      }
+    });
+
+    it('should throw a req bad argument error', function(done) {
+      // setup
+      const mockReq = '';
+      const mockRes = {};
+      const mockNext = function() {};
+
+      // run
+      try {
+        logRequestMiddleware(mockReq, mockRes, mockNext);
+      }
+      catch (err) {
+      // test
+        expect(err).to.be.an('error');
+        expect(err.message).to.equal('Bad argument: req parameter must be an object');
+        return done();
+      }
+    });
+
+    it('should throw a res bad argument error', function(done) {
+      // setup
+      const mockReq = Object.assign({}, mockReqFixture);
+      const mockRes = '';
+      const mockNext = function() {};
+
+      // run
+      try {
+        logRequestMiddleware(mockReq, mockRes, mockNext);
+      }
+      catch (err) {
+      // test
+        expect(err).to.be.an('error');
+        expect(err.message).to.equal('Bad argument: res parameter must be an object');
+        return done();
+      }
+    });
+
+    it('should throw a next bad argument error', function(done) {
+      // setup
+      const mockReq = Object.assign({}, mockReqFixture);
+      const mockRes = {};
+      const mockNext = '';
+
+      // run
+      try {
+        logRequestMiddleware(mockReq, mockRes, mockNext);
+      }
+      catch (err) {
+      // test
+        expect(err).to.be.an('error');
+        expect(err.message).to.equal('Bad argument: next parameter must be a function');
+        return done();
+      }
+    });
+  });
+
+  describe('#throwError', function() {
+    let throwErrorMiddleware, debugStub, utilsStub;
+
+    beforeEach(function() {
+      debugStub = sinon.stub();
+      const debugFake = sinon.fake(function(namespace) {
+        return debugStub;
+      });
+      utilsStub = {
+        getRequestInfo: sinon.stub()
+      };
+      const stubs = {
+        debug: debugFake,
+        '../utils': utilsStub
+      };
+      throwErrorMiddleware = proxyquire('../../lib/middleware/throwError', stubs);
+    });
+
+    it('should throw an error', function() {
+      // setup
+      const mockReq = {};
+      const mockRes = {};
+      const mockNext = sinon.fake(test);
+
+      // run
+      throwErrorMiddleware(mockReq, mockRes, mockNext);
+
+      // test
+      function test(err) {
+        expect(err).to.be.an('error');
+        expect(err.message).to.be.equal('throwing an error!');
       }
     });
   });

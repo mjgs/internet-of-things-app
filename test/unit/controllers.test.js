a9039892a18635cf146a579f365605f17cbadae2
Mark Smith <markjgsmith@gmail.com>
2020-02-15 14:48:41 +0700

    Refactor controller unit tests and add tests for bad params

--- test/unit/controllers.test.js
+++ test/unit/controllers.test.js
@@ -4,28 +4,102 @@ const debug = require('debug')('test:unit:controllers'); // eslint-disable-line 
 
 const expect = require('chai').expect;
 const sinon = require('sinon');
-
-const controllers = require('../../lib/controllers');
+const proxyquire = require('proxyquire');
 
 describe('#controllers', function() {
-  it('should test that the controllers unit test suite is setup', function() {
-    expect(true).to.be.true;
+  describe('test suite', function() {
+    it('should test that the controllers unit test suite is setup', function() {
+      expect(true).to.be.true;
+    });
   });
 
-  it('should render the dashboard', function() {
-    // setup
-    const req = {};
-    const res = {
-      render: sinon.stub()
-    };
-    const next = sinon.stub();
+  describe('#dashboard', function() {
+    let dashboardController, debugStub, utilsStub;
 
-    // run
-    controllers.dashboard(req, res, next);
+    beforeEach(function() {
+      debugStub = sinon.stub();
+      const debugFake = sinon.fake(function(namespace) {
+        return debugStub;
+      });
+      utilsStub = {
+        getRequestInfo: sinon.stub()
+      };
+      const stubs = {
+        debug: debugFake,
+        '../utils': utilsStub
+      };
+      dashboardController = proxyquire('../../lib/controllers/dashboard', stubs);
+    });
 
-    // test
-    expect(res.render.calledOnce).to.be.true;
-    expect(res.render.calledWith('index', { title: 'Dashboard' })).to.be.true;
-    expect(next.notCalled).to.be.true;
+    it('should render the dashboard', function() {
+      // setup
+      const mockReq = {};
+      const mockRes = {
+        render: sinon.stub()
+      };
+      const mockNext = sinon.stub();
+
+      // run
+      dashboardController(mockReq, mockRes, mockNext);
+
+      // test
+      expect(mockRes.render.calledOnce).to.be.true;
+      expect(mockRes.render.calledWith('index', { title: 'Dashboard' })).to.be.true;
+      expect(mockNext.notCalled).to.be.true;
+    });
+
+    it('should throw a req bad argument error', function(done) {
+      // setup
+      const mockReq = '';
+      const mockRes = {};
+      const mockNext = function() {};
+
+      // run
+      try {
+        dashboardController(mockReq, mockRes, mockNext);
+      }
+      catch (err) {
+      // test
+        expect(err).to.be.an('error');
+        expect(err.message).to.equal('Bad argument: req parameter must be an object');
+        return done();
+      }
+    });
+
+    it('should throw a res bad argument error', function(done) {
+      // setup
+      const mockReq = {};
+      const mockRes = '';
+      const mockNext = function() {};
+
+      // run
+      try {
+        dashboardController(mockReq, mockRes, mockNext);
+      }
+      catch (err) {
+      // test
+        expect(err).to.be.an('error');
+        expect(err.message).to.equal('Bad argument: res parameter must be an object');
+        return done();
+      }
+    });
+
+    it('should throw a next bad argument error', function(done) {
+      // setup
+      const mockReq = {};
+      const mockRes = {};
+      const mockNext = '';
+
+      // run
+      try {
+        dashboardController(mockReq, mockRes, mockNext);
+      }
+      catch (err) {
+      // test
+        expect(err).to.be.an('error');
+        expect(err.message).to.equal('Bad argument: next parameter must be a function');
+        return done();
+      }
+    });
   });
 });
